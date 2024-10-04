b019a4af71d1bac51b6a60f8a76255a7bb55015e
Ronan <ronan.jaffre@gmail.com>
2024-10-05 01:26:52 +0200

    a

--- front/src/components/statistics.tsx
+++ front/src/components/statistics.tsx
@@ -5,19 +5,19 @@ const Statistics: React.FC = () => {
       <div className="col-span-1 grid grid-cols-2 gap-4 stats">
         <div className="bg-[#BCCAE1] p-6 rounded-2xl text-black col-span-2">
           <p className="text-black text-lg pb-2">Commissions du mois</p>
-          <h2 className="text-5xl font-semibold">10 000 €</h2>
+          <h2 className="text-5xl font-semibold tracking-1pc">10 000 €</h2>
         </div>
         <div className="bg-[#1E1F21] p-6 rounded-2xl col-span-2">
           <p className="text-gray-200 text-lg pb-2">Commissions en attente</p>
-          <h2 className="text-5xl font-semibold">649 €</h2>
+          <h2 className="text-5xl font-semibold tracking-1pc">649 €</h2>
         </div>
         <div className="bg-[#DCBBDD] p-6 rounded-2xl text-black">
           <p className="text-black-200 text-lg pb-2">Produits actifs</p>
-          <h2 className="text-4xl font-semibold">53</h2>
+          <h2 className="text-4xl font-semibold tracking-1pc">53</h2>
         </div>
         <div className="bg-[#1E1F21] p-6 rounded-2xl">
           <p className="text-gray-200 text-lg pb-2">Évolution des commissions</p>
-          <h2 className="text-4xl font-semibold">+25%</h2>
+          <h2 className="text-4xl font-semibold tracking-1pc">+25%</h2>
         </div>
       </div>
     );
