69fb6adaac4c493a2fa21acb67f7226668e5d08d
Ronan <ronan.jaffre@gmail.com>
2024-10-05 20:28:33 +0200

    a

--- front/src/pages/statisticsPage.tsx
+++ front/src/pages/statisticsPage.tsx
@@ -1,12 +1,30 @@
-import CommissionsList from "../components/apporteurs/commissionsList";
 import Container from "../components/container";
-import Header from "../components/apporteurs/header";
-import Statistics from "../components/apporteurs/statistics";
+import HeaderA from "../components/apporteurs/header";
+import StatisticsA from "../components/apporteurs/statistics";
+import CommissionsListA from "../components/apporteurs/commissionsList";
+import HeaderF from "../components/fournisseurs/header";
+import StatisticsF from "../components/fournisseurs/statistics";
+import CommissionsListF from "../components/fournisseurs/commissionsList";
+import { useUserContext } from "../context/user.context";
 
-export const StatisticsPage = () => <Container>
-    <Header />
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-6">
-        <Statistics />
-        <CommissionsList />
-    </div>
-</Container>
+export const StatisticsPage = () => {
+    const { currentUser } = useUserContext();
+
+    const renderApporteur = () => <Container>
+        <HeaderA />
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-6">
+            <StatisticsA />
+            <CommissionsListA />
+        </div>
+    </Container>
+
+    const renderFournisseur = () => <Container>
+        <HeaderF />
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-6">
+            <StatisticsF />
+            <CommissionsListF />
+        </div>
+    </Container>
+
+    return currentUser?.role === 1 ? renderApporteur() : renderFournisseur();
+}
